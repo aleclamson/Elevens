6d59c846dda7e01790ff45075bfe95e6b7afccb2
Alec Lamson <lamsonak@s.dcsdk12.org>
2016-04-13 10:01:00 -0600

    done with a1

--- src/CardTester.java
+++ src/CardTester.java
@@ -8,6 +8,10 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card card1 = new Card("Heart", "9", 9);
+		Card card2 = new Card("Heart", "9", 9);
+		Card card3 = new Card("Heart", "Jack", 11);
+		System.out.println(card3.toString());
+
 	}
 }
