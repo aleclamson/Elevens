a15275d80ed756e18a5d8c4e73fa0f40b6cbde3f
Alec Lamson <lamsonak@s.dcsdk12.org>
2016-04-26 09:45:55 -0600

    done with a4

--- src/Deck.java
+++ src/Deck.java
@@ -62,7 +62,12 @@ public class Deck {
 	 * and reset the size to represent the entire deck.
 	 */
 	public void shuffle() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 4 *** */
+		   for (int k = size() - 1; k >= 1; k--) {
+		    	  int r = (int) (Math.random() * (k + 1));
+		          Card temp = cards.get(k);
+		          cards.set(k, cards.get(r));
+		          cards.set(r, temp);
+		   }
 	}
 
 	/**
